ee388b0dd5b7c481f370ccb9d1a01bfc5d5d0b63
Molly Lau <windowsbeta@outlook.com>
2018-08-09 22:18:54 +0800

    Update netopt.sh

--- netopt.sh
+++ netopt.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
+# 判断系统虚拟化技术
+
+checkvirtual(){
+	if [ -d /proc/vz ]; then
+	return virtual="ovz"
+	elif [ -d /proc/xen ]; then
+	return virtual="xen"
+	else
+	return virtual="kvm"
+	fi
+	echo $virtual
+}
+
+checkvirtual
+
 echo "ulimit -SHn 1024000" >> /etc/profile
 ulimit -n 1024000
 echo "* soft nofile 1024000" >> /etc/security/limits.conf
 echo "* hard nofile 1024000" >> /etc/security/limits.conf
-/sbin/modprobe tcp_hybla
 
 read -n1 -p  "已安装增强（魔改）版的BBR？(y/n)" ans
 if [[ ${ans} =~ [yY] ]]; then
+
+read -p "请选择你的位置和服务器之间的距离，处于同一洲按1，跨洲按2（ovz和xen架构机器无法开启hybla算法）" pick
+[ -z "$pick" ]
+expr ${pick} + 1 &>/dev/null
+if [ $? -ne 0 ]; then
+        echo -e "Input error, please input a number"
+        continue
+fi
+
+if [[ ${virtual} == "ovz" ]] || [[ ${virtual} == "xen" ]] && [[ "${pick" == 1 ]]; then
+	tcp_type="cubic"
+else
+	tcp_type="hybla"
+fi
+
+/sbin/modprobe tcp_$tcp_type
+	
 	cat > /etc/sysctl.conf<<-EOF
 # max open files
 fs.file-max = 1024000
@@ -48,7 +79,7 @@ net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.tcp_mtu_probing = 1
 
 # for high-latency network
-net.ipv4.tcp_congestion_control = hybla
+net.ipv4.tcp_congestion_control = $tcp_type
 # forward ipv4
 net.ipv4.ip_forward = 1
 EOF
